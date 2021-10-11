8032ad892925639edde0f6d195d2b2be6c0209cb
Pavel Dubov <PADking888@yandex.ru>
2021-10-11 20:37:09 +0300

    feat: Restyle code to a single style

--- property/models.py
+++ property/models.py
@@ -6,50 +6,40 @@ from phonenumber_field.modelfields import PhoneNumberField
 
 
 class Flat(models.Model):
-    new_building = models.NullBooleanField(
-        'Является новостройкой', db_index=True)
+    new_building = models.NullBooleanField('Является новостройкой',
+                                           db_index=True)
 
-    created_at = models.DateTimeField(
-        'Когда создано объявление',
-        default=timezone.now,
-        db_index=True)
+    created_at = models.DateTimeField('Когда создано объявление',
+                                      default=timezone.now,
+                                      db_index=True)
 
     description = models.TextField('Текст объявления', blank=True)
     price = models.IntegerField('Цена квартиры', db_index=True)
+    town = models.CharField('Город, где находится квартира',
+                            max_length=50,
+                            db_index=True)
 
-    town = models.CharField(
-        'Город, где находится квартира',
-        max_length=50,
-        db_index=True)
-    town_district = models.CharField(
-        'Район города, где находится квартира',
-        max_length=50,
-        blank=True,
-        help_text='Чертаново Южное')
-    address = models.TextField(
-        'Адрес квартиры',
-        help_text='ул. Подольских курсантов д.5 кв.4')
-    floor = models.CharField(
-        'Этаж',
-        max_length=3,
-        help_text='Первый этаж, последний этаж, пятый этаж')
+    town_district = models.CharField('Район города, где находится квартира',
+                                     max_length=50, blank=True,
+                                     help_text='Чертаново Южное')
 
-    rooms_number = models.IntegerField(
-        'Количество комнат в квартире',
-        db_index=True)
-    living_area = models.IntegerField(
-        'количество жилых кв.метров',
-        null=True,
-        blank=True,
-        db_index=True)
+    address = models.TextField('Адрес квартиры',
+                               help_text='ул. Подольских курсантов д.5 кв.4')
+
+    floor = models.CharField('Этаж', max_length=3,
+                             help_text='Первый этаж, последний этаж, пятый этаж')
+
+    rooms_number = models.IntegerField('Количество комнат в квартире',
+                                       db_index=True)
+
+    living_area = models.IntegerField('количество жилых кв.метров',
+                                      null=True, blank=True, db_index=True)
 
     has_balcony = models.NullBooleanField('Наличие балкона', db_index=True)
     active = models.BooleanField('Активно-ли объявление', db_index=True)
-    construction_year = models.IntegerField(
-        'Год постройки здания',
-        null=True,
-        blank=True,
-        db_index=True)
+    construction_year = models.IntegerField('Год постройки здания',
+                                            null=True, blank=True,
+                                            db_index=True)
 
     liked_by = models.ManyToManyField(User, blank=True,
                                       related_name='liked_flats',
@@ -79,6 +69,7 @@ class Owner(models.Model):
     owners_phonenumber = models.CharField('Номер владельца', max_length=20)
     owner_pure_phone = PhoneNumberField('Нормализованный номер владельца',
                                         blank=True, db_index=True)
+
     flats = models.ManyToManyField(Flat, related_name='owners',
                                    verbose_name='Квартиры в собственности')
 
